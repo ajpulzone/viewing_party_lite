7ff346ffe2b8ce383c1c502266ce166ff5b5e9eb
Annie Pulzone <ajpulzone@gmail.com>
2022-12-15 11:41:00 -0800

    Updated tests to account for a landing page view for a visitor vs logged in user, deleted tests that were no longer needed, refactored landing page view

--- spec/features/landing_page/index_spec.rb
+++ spec/features/landing_page/index_spec.rb
@@ -2,44 +2,50 @@ require 'rails_helper'
 
 RSpec.describe 'Landing Page' do
   before(:each) do
-   @user1 = User.create!(name: 'Chad', email: 'chad1@gmail.com', password: "1234test", password_confirmation: "1234test")
+    @user1 = User.create!(name: 'Chad', email: 'chad1@gmail.com', password: "1234test", password_confirmation: "1234test")
     @user2 = User.create!(name: 'Jessica', email: 'jessica2@gmail.com', password: "1234test", password_confirmation: "1234test")
     @user3 = User.create!(name: 'Fiona', email: 'Fiona3@gmail.com', password: "1234test", password_confirmation: "1234test")
   end
 
-  it 'should contain title of Viewing Party' do
-    visit root_path
-    within('#dashboard') do
-      expect(page).to have_content('Viewing Party')
+  describe "Landing Page seen by a visitor" do
+    it 'should contain title of Viewing Party' do
+      visit root_path
+      within('#dashboard') do
+        expect(page).to have_content('Viewing Party')
+      end
+    end
+
+    it 'should contain link titled create a new user' do
+      visit root_path
+    
+      expect(page).to have_link('Create A User')
+      click_link('Create A User')
+      expect(current_path).to eq("/register")
+    end
+
+    it 'should NOT contain a list of existing users' do
+      visit root_path
+      expect(page).to have_no_content(@user1.name)
+      expect(page).to have_no_content(@user2.name)
+      expect(page).to have_no_content(@user3.name)
     end
   end
 
-  it 'should contain button titled create new user' do
-    visit root_path
-  
-    expect(page).to have_link('Create A User')
-    click_link('Create A User')
-    expect(current_path).to eq("/register")
-  end
+  describe "Landing Page seen as a logged in user" do
+    it 'if a user is logged in the landing page should contain a list of existing users e-mail addresses' do
+      visit login_path
 
-  it 'should contain a list of existing users' do
-    visit root_path
-    within('#existing_users') do
-      expect(page).to have_content(@user1.name)
-      expect(page).to have_content(@user2.name)
-      expect(page).to have_content(@user3.name)
-      expect(page).to have_no_content('Bob')
-    end
-  end
+      fill_in :email, with: "chad1@gmail.com"
+      fill_in :password, with: "1234test"
+      click_button "Login" 
 
-  xit 'the name of each existing user link to that users dashboard' do
-    visit root_path
-    within('#existing_users') do
-      expect(page).to have_link(@user1.name)
-      expect(page).to have_link(@user2.name)
-      expect(page).to have_link(@user3.name)
-      click_link @user1.name
-      expect(current_path).to eq(user_path(@user1.id))
+      visit root_path 
+
+      within('#existing_users') do
+        expect(page).to have_content(@user1.email)
+        expect(page).to have_content(@user2.email)
+        expect(page).to have_content(@user3.email)
+      end
     end
   end
 
@@ -51,4 +57,4 @@ RSpec.describe 'Landing Page' do
       expect(current_path).to eq(root_path)
     end
   end
-end
+end 
